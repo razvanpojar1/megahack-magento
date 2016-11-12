fa0a28ae365d2e96276c6a7da57fb2e6ade17264
Razvan Pojar <pojarrazvan@gmail.com>
2016-11-12 18:03:47 +0200

    CRUD warehouse functionality

--- app/code/community/MagentoCrew/Warehouse/controllers/Adminhtml/WarehouseController.php
+++ app/code/community/MagentoCrew/Warehouse/controllers/Adminhtml/WarehouseController.php
@@ -35,8 +35,101 @@ class MagentoCrew_Warehouse_Adminhtml_WarehouseController extends Mage_Adminhtml
         $this->_title($this->__('Manage Warehouse'));
         $this->_setActiveMenu('Manage Warehouse');
         $this->_initAction()
-            ->_addContent($this->getLayout()->createBlock('mc_warehouse/adminhtml_warehouse'))
+            ->_addContent($this->getLayout()->createBlock('mc_warehouse/adminhtml_warehouse_warehouse'))
             ->renderLayout();
+    }
+
+    /**
+     * Forward to edit action
+     */
+    public function newAction()
+    {
+        $this->_forward('edit');
+    }
+
+    /**
+     * Render index action
+     *
+     * @return null
+     */
+    public function editAction()
+    {
+        $this->_title($this->__('Warehouse edit'));
+
+        $warehouseId = $this->getRequest()->getParam('id');
+        $warehouseModel  = Mage::getModel('mc_warehouse/warehouse')->load($warehouseId);
+
+        if ($warehouseModel->getId() || $warehouseId == 0) {
+            $this->_title($warehouseModel->getId() ? $warehouseModel->getName() : $this->__('New warehouse'));
+
+            Mage::register('warehouse_data', $warehouseModel);
+            $this->loadLayout();
+
+            $this->_setActiveMenu('Edit Warehouse');
+            $this->_addBreadcrumb(Mage::helper('mc_warehouse')->__('Warehouse Manager'), Mage::helper('mc_warehouse')->__('Warehouse Manager'), $this->getUrl('*/*/'));
+            $this->_addBreadcrumb(Mage::helper('mc_warehouse')->__('Edit Warehouse'), Mage::helper('mc_warehouse')->__('Edit Warehouse'));
+
+            $this->getLayout()->getBlock('head')->setCanLoadExtJs(true);
+            $this->_addContent($this->getLayout()->createBlock('mc_warehouse/adminhtml_warehouse_edit'))
+                ->_addLeft($this->getLayout()->createBlock('mc_warehouse/adminhtml_warehouse_edit_tabs'));
+
+            $this->renderLayout();
+        } else {
+            Mage::getSingleton('adminhtml/session')->addError(Mage::helper('mc_warehouse')->__('The warehouse does not exist.'));
+            $this->_redirect('*/*/');
+        }
+    }
+
+    public function saveAction()
+    {
+        if ($data = $this->getRequest()->getPost()) {
+
+            $warehouseModel = Mage::getModel('mc_warehouse/warehouse');
+
+            if ($warehouseId = $this->getRequest()->getParam('id')) {
+                $warehouseModel->setData($data)
+                    ->setId($warehouseId);
+            } else {
+                $warehouseModel->setData($data);
+            }
+
+            try {
+                $warehouseModel->save();
+
+                if ($this->getRequest()->getParam('back')) {
+                    $this->_redirect('*/*/edit', array('id' => $warehouseModel->getId()));
+                    return;
+                }
+
+                Mage::getSingleton('adminhtml/session')->addSuccess('Warehouse successfully saved');
+                $this->_redirect('*/*/');
+            }
+            catch (Exception $e){
+                Mage::getSingleton('adminhtml/session')->addError('An error has occured. Please try again');
+                Mage::getSingleton('adminhtml/session')->setExampleFormData($data);
+                $this->_redirect('*/*/edit', array('id' => $warehouseModel->getId(), '_current'=>true));
+            }
+        }
+    }
+
+    public function deleteAction()
+    {
+        if ($id = $this->getRequest()->getParam('id')) {
+            try {
+                $model = Mage::getModel('mc_warehouse/warehouse')->load($id);
+                $model->delete();
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('mc_warehouse')->__('The warehouse has been deleted.'));
+                $this->_redirect('*/*/');
+                return;
+            }
+            catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+                return;
+            }
+        }
+        Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Unable to find a warehouse to delete.'));
+        $this->_redirect('*/*/');
     }
 
     /**
@@ -63,7 +156,11 @@ class MagentoCrew_Warehouse_Adminhtml_WarehouseController extends Mage_Adminhtml
         $this->_prepareDownloadResponse($fileName, $grid->getExcelFile($fileName));
     }
 
-
+    /**
+     * Check if module is allowed
+     *
+     * @return bool
+     */
     protected function _isAllowed()
     {
         return Mage::getSingleton('admin/session')->isAllowed('warehouse');
